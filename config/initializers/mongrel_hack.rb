86635b6bdeff12e8ee86f449a2ee65a90005edb1
Gareth Jones <gareth.e.jones@gmail.com>
2010-09-08 11:47:23 -0500

    hacking the mongrel hack... christ.

--- config/initializers/mongrel_hack.rb
+++ config/initializers/mongrel_hack.rb
@@ -1,4 +1,5 @@
-if Rails.version == '2.3.8' && Gem.available?('mongrel', Gem::Requirement.new('~>1.1.5')) && self.class.const_defined?(:Mongrel)
+#was: if Rails.version == '2.3.8' && Gem.available?('mongrel', Gem::Requirement.new('~>1.1.5')) && self.class.const_defined?(:Mongrel)
+if Rails.version == '2.3.8' && Gem.available?('mongrel', '~>1.1.5') && self.class.const_defined?(:Mongrel)
   
   # Pulled right from latest rack. Old looked like this in 1.1.0 version.
   # 
