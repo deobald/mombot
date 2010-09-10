bfad8847fabe3e77c975afa2d25e7fb47fc1c684
Gareth Jones <gareth.e.jones@gmail.com>
2010-09-10 17:44:39 -0500

    a bit o cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,32 +10,30 @@ class ApplicationController < ActionController::Base
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
   
-  def with_login_requirement message
-    # TODO: next
+  def with_login_requirement failure_message
+    if yield
+      return true
+    end
+    flash[:warning] = failure_message
+    session[:return_to] = request.request_uri
+    redirect_to :controller => "users", :action => "login"
+    return false
   end
   
   def login_required
-    if current_user
-      return true
-    end
-    flash[:warning] = 'Please login to continue'
-    session[:return_to] = request.request_uri
-    redirect_to :controller => "users", :action => "login"
-    return false
+    with_login_requirement('Please login to continue') { current_user }
   end
   
   def admin_login_required
-    if current_user && current_user.admin?
-      return true
-    end
-    flash[:warning] = 'Please login as an administrator to continue'
-    session[:return_to] = request.request_uri
-    redirect_to :controller => "users", :action => "login"
-    return false
+    with_login_requirement('Please login as an administrator to continue') { current_user_is_admin? }
   end
 
   def current_user
     session[:user]
+  end
+  
+  def current_user_is_admin?
+    current_user && current_user.admin?
   end
 
   def return_to_stored
