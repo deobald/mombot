74d3af3c18154da0f1436ab6b82e1aef173ea8af
Steven Deobald <steven@deobald.ca>
2010-09-14 16:55:07 -0500

    seated pezez show their images

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,6 @@
 
 class Image < ActiveRecord::Base
+  belongs_to :pez
   has_attachment :content_type => :image, :storage => :file_system, 
                  :path_prefix => 'public/attachments', :max_size => 1.megabyte
                  
